08a78c15ed0d2dede4bbc35c29a6ba9565a11c5f
Giorgos Andreas Michas <114501104+Giorosm@users.noreply.github.com>
2024-04-03 14:57:17 +0300

    Add files via upload

--- saucedemo_test3.py
+++ saucedemo_test3.py
@@ -47,23 +47,7 @@ class TestSauceDemo(unittest.TestCase):
         continue_button.click()
         time.sleep(4)
 
-        # finish
-        finish_button = self.driver.find_element(By.ID, "finish")
-        finish_button.click()
-        time.sleep(4)# fill the registration form and submit
-        firstName_input = self.driver.find_element(By.ID,"first-name")
-        firstName_input.send_keys("john")
-
-        lastName_input = self.driver.find_element(By.ID,"last-name")
-        lastName_input.send_keys("smith")
-
-        zipCode_input = self.driver.find_element(By.ID,"postal-code")
-        zipCode_input.send_keys("123456")
-
-        continue_button = self.driver.find_element(By.ID,"continue")
-        continue_button.click()
-        time.sleep(4)
-
+        
         # finish
         finish_button = self.driver.find_element(By.ID, "finish")
         finish_button.click()
@@ -73,8 +57,8 @@ class TestSauceDemo(unittest.TestCase):
         #WebDriverWait(self.driver, 10).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "title"), "THANK YOU FOR YOUR ORDER"))
         
         #def thankYou_page(self):
-        thank_you_text = self.driver.find_element(By.XPATH,"//h2[text()='THANK YOU FOR YOUR ORDER']").text
-        self.assertEqual(thank_you_text, "THANK YOU FOR YOUR ORDER", "Thank you page not displayed")
+        thank_you_text = self.driver.find_element(By.XPATH,"//h2[@data-test='complete-header']").text
+        self.assertEqual(thank_you_text, "Thank you for your order!", "Thank you page not displayed")
         time.sleep(5)
 
     def tearDown(self):
